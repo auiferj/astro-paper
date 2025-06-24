d104aa4c6872112fec9e68a44820567085a0f38a
auiferj <auiferj@163.com>
2025-06-24 23:02:00 +0800

    bits

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: "Sat Naing",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "山居 | Feed",
+  title: "山居客 | Feed",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
