ca39c220adedb1dce3b295aa34ca6dad91d16fae
zhangwei <zhangwei@qq.com>
2025-06-30 14:41:34 +0800

    bits

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: "Sat Naing",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "山居 | 苔痕",
+  title: "橙光补给站",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
